7d82f292772661a17c27ea038b2d3a98321da084
zaidsheriff786 <mszaid.007@gmail.com>
2022-09-21 15:42:27 +0530

    initial commit

---
+++ src/DigitButton.js
@@ -0,0 +1,12 @@
+import { ACTIONS } from './App'
+
+export default function DigitButton({ dispatch, digit }) {
+  return (
+    <button
+      className="button-wrapper"
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  )
+}
